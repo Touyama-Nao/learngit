0605a44f1b56abdff665e9d8c86b1e8541b6a140
滨边家的小美波 <3096237774@qq.com>
2018-07-20 19:43:24 +0800

    提交

--- 主页介绍扁平化.js
+++ 主页介绍扁平化.js
@@ -12,13 +12,16 @@ $(function(){
             if(i<=3){
                 $("#x-slider-pannel-left-carouse").css("margin-left",(-i*800)+"px");
                 i++;
+                $(".slider-dot").eq(i-1).css("background-color","yellow");
             }else if(i==4){
                 i=0;
+                $(".slider-dot").eq(i-1).css("background-color","white");
+                $(".slider-dot").eq(i-2).css("background-color","yellow")
             }
             return t;
     }
     } ;
-    setInterval(a.fn,3000); 
+    var settime=setInterval(a.fn,3000); 
 })
 $(function(){
     var i=0;
@@ -34,13 +37,15 @@ $(function(){
             if(i<=3){
                 $("#x-slider-pannel-center1-carouse").css("margin-top",(-i*600)+"px");
                 i++;
+                $(".slider-dot").eq(i-1).css("background-color","white");
+                $(".slider-dot").eq(i-2).css("background-color","yellow")
             }else if(i==4){
                 i=0;
             }
             return t;
     }
     } ;
-    setInterval(a.fn,3000); 
+    var settime=setInterval(a.fn,3000); 
 })
 $(function(){
     var i=0;
@@ -56,13 +61,15 @@ $(function(){
             if(i<=3){
                 $("#x-slider-pannel-center2-carouse").css("margin-top",(i*600-1200)+"px");
                 i++;
+                $(".slider-dot").eq(i-2).css("background-color","white");
+                $(".slider-dot").eq(i-1).css("background-color","white");
             }else if(i==4){
                 i=0;
             }
             return t;
     }
     } ;
-    setInterval(a.fn,3000); 
+    var settime=setInterval(a.fn,3000); 
 })
 $(function(){
     var i=0;
@@ -78,11 +85,29 @@ $(function(){
             if(i<=3){
                 $("#x-slider-pannel-right-carouse").css("margin-left",(i*800-2200)+"px");
                 i++;
+                $(".slider-dot").eq(i-2).css("background-color","white");
+                $(".slider-dot").eq(i-1).css("background-color","yellow");
             }else if(i==4){
                 i=0;
+                
             }
             return t;
     }
     } ;
-    setInterval(a.fn,3000); 
+   var settime= setInterval(a.fn,3000); 
+})
+$(function(){
+    var b={
+        b1: ".slider-dot",
+        fn2: function(){
+            var t = this;
+            $(t.b1).mouseover(function(){
+                console.log("hello")
+                clearInterval(settime);
+                $(t.b1).css("background","white");
+                $(this).css("background-color","yellow");
+
+            })
+        }
+    }.fn2();
 })
