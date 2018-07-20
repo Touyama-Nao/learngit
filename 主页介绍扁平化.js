5c93a8ce371bccbffbd40067457938f618176bf4
滨边家的小美波 <3096237774@qq.com>
2018-07-20 17:36:46 +0800

    20日17点30分

--- 主页介绍扁平化.js
+++ 主页介绍扁平化.js
@@ -1,5 +1,21 @@
 $(function(){
+    var i=0;
     var a={
+        a1:"#x-slider-pannel-left-carouse",
+        a2:"",
         
+        fn: function(){
+            if(i==3){
+                i=0;
+            }
+            console.log(i);
+            var t= this;
+            for( i=0;i<3;i++){
+                $("#x-slider-pannel-left-carouse").css("margin-left" ,(-i*800)+"px");
+            console.log(i);
+        }
+            return t;
     }
+    } ;
+    setInterval(a.fn,1000); 
 })
